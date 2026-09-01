97c10fd08a92b9378aa3f95cb176b38aee6b084f
Roger-Manser <roger@scheese.ch>
2026-09-01 08:04:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Version: 1.0.4 (30.08.2026)
 // Update CACHE_NAME mit jeder neuen Version
-const CACHE_NAME = 'geraete-v1.0.37';
+const CACHE_NAME = 'geraete-v1.0.38';
 const ASSETS = [
   '/geraete/',
   '/geraete/index.html',
